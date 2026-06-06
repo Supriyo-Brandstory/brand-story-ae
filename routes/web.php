d0e93cfdf4ba1d8ff1667488452c3b8b0e38cf45
supriyo-brandstory <supriyo@brandstory.in>
2026-06-06 10:13:43 +0530

    ..

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Core\Route;
 Route::get('/thankyou', 'FrontendController@thankYou', 'thankyou');
 Route::get('/old-site/*', 'FrontendController@notfound', 'old-site');
 Route::get('/', 'FrontendController@index', 'home');
-Route::get('/home-2', 'FrontendController@index2', 'home2');
+// Route::get('/home-2', 'FrontendController@index2', 'home2');
 Route::get('/about', 'FrontendController@about', 'about');
 Route::get('/author/madhavan-a', 'FrontendController@authorMadhavan', 'author.madhavan');
 Route::get('/contact', 'FrontendController@contat', 'contact');
