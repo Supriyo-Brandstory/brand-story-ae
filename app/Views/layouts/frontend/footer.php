263f72c60878e2b45bf651c5a0d91416690f1e5a
supriyo-brandstory <supriyo@brandstory.in>
2026-08-05 19:31:25 +0530

    ...

--- app/Views/layouts/frontend/footer.php
+++ app/Views/layouts/frontend/footer.php
@@ -1,115 +1,165 @@
-<section class="footer-top sp-50"><!--Footer top start-->
-  <section class="footer-partners"><!--Footer partners start-->
-    <div class="container"><!--Container Start-->
-      <div class="partners-grid-wrap">
-        <div class="partner-card">
+<footer class="site-footer-wrapper">
+  <!-- Top Left SVG Glow Light Asset -->
+  <div class="ftr-glow-top-left" aria-hidden="true">
+    <svg width="337" height="438" viewBox="0 0 337 438" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g opacity="0.5" filter="url(#filter0_f_1689_2727)">
+        <path d="M257 210.021L24.5577 -151H-202L179.912 358L93.9962 128.259L257 210.021Z" fill="white"/>
+        <path d="M211.239 168.298L14.5566 -137.182H-177.146L146.01 293.511L73.3122 99.1145L211.239 168.298Z" fill="#855BFF"/>
+        <path d="M162.846 119.774L-3.33427 -138.332H-165.308L107.733 225.568L46.3095 61.3192L162.846 119.774Z" fill="white"/>
+      </g>
+      <defs>
+        <filter id="filter0_f_1689_2727" x="-282" y="-231" width="619" height="669" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+          <feGaussianBlur stdDeviation="40" result="effect1_foregroundBlur_1689_2727"/>
+        </filter>
+      </defs>
+    </svg>
+  </div>
+
+  <!-- Bottom Right SVG Glow Light Asset -->
+  <div class="ftr-glow-bottom-right" aria-hidden="true">
+    <svg width="363" height="433" viewBox="0 0 363 433" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_f_1689_2712)">
+        <path d="M80.0001 255.398L151.015 462.564L169.985 517.903L389.922 442.51L304.152 192.302L196.942 79.9991L238.608 257.056L159.329 175.348L165.375 310.173L80.0001 255.398Z" fill="#855BFF"/>
+        <path d="M91.5262 272.061L161.65 467.376L178.407 513.175L383.681 442.809L310.799 230.195L219.836 131.879L254.459 284.69L182.892 215.51L185.484 332.445L91.5262 272.061Z" fill="#B55BFF"/>
+        <path d="M112.422 294.47L174.915 468.234L189.857 508.976L372.246 446.455L314.488 254.833L241.019 168.488L267.795 305.192L197.017 243.019L198.208 347.453L112.422 294.47Z" fill="#E65BFF"/>
+        <path d="M130.023 318.064L181.907 469.418L201.576 504.96L367.181 448.192L314.85 279.02L257.409 199.341L280.253 319.951L207.834 272.52L208.875 363.793L130.023 318.064Z" fill="white"/>
+      </g>
+      <defs>
+        <filter id="filter0_f_1689_2712" x="0" y="0" width="469.922" height="597.902" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+          <feGaussianBlur stdDeviation="40" result="effect1_foregroundBlur_1689_2712"/>
+        </filter>
+      </defs>
+    </svg>
+  </div>
+
+  <!-- Top Partner Logos Bar -->
+  <section class="footer-partners-bar">
+    <div class="container">
+      <div class="partners-grid-row">
+        <div class="partner-col">
           <a href="https://www.google.com/partners/agency?id=1975289574" target="_blank" rel="noopener">
-            <img src="/assets/images/hover-google.svg" class="img-fluid" alt="google-partners">
+            <img src="/assets/images/hover-google.svg" class="partner-logo-img" alt="Google Partner">
           </a>
         </div>
-        <div class="partner-card">
-          <img src="/assets/images/Ahrefs-Agency-Parnter-SEO.png" class="img-fluid" alt="Ahrefs-Agency-Parnter-SEO">
+        <div class="partner-col">
+          <img src="/assets/images/hover-meta.svg" class="partner-logo-img" alt="Meta Business Partner">
         </div>
-        <div class="partner-card">
-          <img src="/assets/images/hover-meta.svg" class="img-fluid" alt="facebook-marketing-partners">
+        <div class="partner-col">
+          <img src="/assets/images/Ahrefs-Agency-Parnter-SEO.png" class="partner-logo-img" alt="Ahrefs Agency Partner">
         </div>
-        <div class="partner-card">
-          <img src="/assets/images/hover-shopify.svg" class="img-fluid" alt="hubspot-partners">
+        <div class="partner-col">
+          <img src="/assets/images/hover-shopify.svg" class="partner-logo-img" alt="Shopify Development Partner">
         </div>
-        <div class="partner-card">
-          <img src="/assets/images/hover-tiktok.svg" class="img-fluid" alt="hubspot-partners">
+        <div class="partner-col">
+          <img src="/assets/images/hover-tiktok.svg" class="partner-logo-img" alt="TikTok Marketing Partner">
         </div>
-
       </div>
-    </div><!--Container end-->
-  </section><!--Footer partners end-->
-  <div class="container"><!--Container Start-->
-    <div class="row gx-lg-4">
-      <div class="col-md-12 col-lg-3">
-        <div class="ftr-logo">
-          BRANDSTORY
+    </div>
+  </section>
+
+  <!-- Main Footer Content Grid -->
+  <div class="footer-main-content">
+    <div class="container">
+      <div class="row gx-lg-5 gy-4">
+        <!-- Col 1: Brand Info & Socials -->
+        <div class="col-lg-3 col-md-12">
+          <div class="ftr-brand-title">BRANDSTORY <sup>®</sup></div>
+          <p class="ftr-brand-desc">
+            BrandStory is a premier digital transformation agency in Dubai. We excel in digital marketing, web design, and branding expertise to help UAE-based businesses connect, expand, and succeed.
+          </p>
+          <div class="ftr-social-block">
+            <h6 class="ftr-social-title">Follow Us</h6>
+            <div class="ftr-social-icons">
+              <a href="https://www.linkedin.com/company/brandstoryae/" target="_blank" rel="noopener" aria-label="LinkedIn" class="social-icon-link">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.28 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.75M6.46 10.9v8.37H9.25V10.9H6.46M7.86 6.58a1.6 1.6 0 0 0-1.6 1.6c0 .88.71 1.6 1.6 1.6.89 0 1.6-.72 1.6-1.6 0-.88-.71-1.6-1.6-1.6Z"/></svg>
+              </a>
+              <a href="https://www.facebook.com/brandstoryuae/" target="_blank" rel="noopener" aria-label="Facebook" class="social-icon-link">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.99 3.66 9.12 8.44 9.88v-6.99H7.9v-2.89h2.54V9.8c0-2.51 1.49-3.89 3.78-3.89 1.09 0 2.23.19 2.23.19v2.47h-1.26c-1.24 0-1.63.77-1.63 1.56v1.88h2.78l-.44 2.89h-2.34v6.99C18.34 21.12 22 16.99 22 12z"/></svg>
+              </a>
+              <a href="https://www.instagram.com/brandstorydubai/" target="_blank" rel="noopener" aria-label="Instagram" class="social-icon-link">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg>
+              </a>
+              <a href="https://www.youtube.com/@brandstoryuae7649" target="_blank" rel="noopener" aria-label="YouTube" class="social-icon-link">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>
+              </a>
+              <a href="https://x.com/BrandStory_UAE" target="_blank" rel="noopener" aria-label="X (Twitter)" class="social-icon-link">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
+              </a>
+            </div>
+          </div>
         </div>
-        <p class="ftr-desc">
-          BrandStory is a premier digital transformation agency in Dubai. We excel in digital marketing, web design, and branding expertise to help UAE-based businesses connect, expand, and succeed.
 
-        </p>
-        <!-- <div class="ftr-btn"><a href="/contact/" class="btn">Contact Sales</a></div>
-        <p><b>BrandStory UAE</b><br>
-          Proudly celebrating 14 passionate years as a trusted digital marketing partner to 350+ clients.
-        </p> -->
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3608.2277486384696!2d55.332991899999996!3d25.262922999999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6d3d63755bdb%3A0xcb6dc8a6ec14f4a2!2sBrandStory%20%7C%20Marketing%20Agency%20in%20Dubai!5e0!3m2!1sen!2sin!4v1779289278967!5m2!1sen!2sin" width="90%" height="150" style="border:0; border-radius:10px; margin-top:20px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-      </div>
-      <div class="col-md-4 col-lg-3">
-        <h5 class="ftr-sub-title">Useful Links</h5>
-        <div class="ftr-links-col">
-          <ul class="ftr-links">
-            <li><a href="/">Home</a>
-            </li>
-            <li><a href="/about/">About Us</a>
-            </li>
-            <li><a href="/services/">Our Services </a></li>
-
-            <li><a href="/contact/">Contact Us </a>
-            </li>
-            <li><a href="/case-study/">Case Studies </a></li>
+        <!-- Col 2: Quick Links -->
+        <div class="col-lg-3 col-md-4">
+          <h5 class="ftr-col-heading">Quick links</h5>
+          <ul class="ftr-nav-links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about/">About Us</a></li>
+            <li><a href="/services/">Our services</a></li>
+            <li><a href="/contact/">Contact Us</a></li>
+            <li><a href="/case-study/">Case Studies</a></li>
             <li><a href="/blog/">Latest Blogs</a></li>
             <li><a href="/careers/">Careers</a></li>
             <li><a href="/digital-cost-calculator/">Digital Cost Calculator</a></li>
             <li><a href="/seo-calculator/">SEO Calculator</a></li>
           </ul>
         </div>
-      </div>
-      <div class="col-md-4 col-lg-3 ftr-service-col">
-        <h5 class="ftr-sub-title">Our Services</h5>
-        <ul class="ftr-links">
-          <li><a href="/">Digital Marketing</a></li>
-          <li><a href="/seo-services-company-in-dubai/">Search Engine Optimization</a></li>
-          <li><a href="/social-media-marketing-agency-in-dubai/">Social Media Marketing</a></li>
-          <li><a href="/pay-per-click-ppc-services-in-dubai/">Pay-Per-Click Ads</a></li>
-          <li><a href="/email-marketing-company-in-dubai/">Email Marketing</a>
-          </li>
-          <li><a href="/branding-agency-in-dubai/">Branding Services</a></li>
-          <li><a href="/website-design-company-in-dubai/">Web Design & Development</a></li>
-          <li><a href="/ui-ux-design-company-in-dubai/">UI UX Design</a></li>
-        </ul>
-      </div>
-      <div class="col-6 col-md-2 col-lg-2">
-        <h5 class="ftr-sub-title">Contact</h5>
-        <div class="ftr-india">Phone: <br><a href="tel:+971-522831655">+971 52 283 1655</a></div>
-        <div class="ftr-email">Email:<br><a href="mailto:info@brandstory.ae">info@brandstory.ae</a></div>
-        <div class="ftr-adrr">
-          <p>Address:<br><a
-              href="https://g.co/kgs/4bHRtr4"
-              target="_blank">G5, Al Meheri Plaza, opp DBC Building, Al Khabaisi Area, Deira Dubai - 81577, United Arab
-              Emirates</a></p>
+
+        <!-- Col 3: Our Services -->
+        <div class="col-lg-3 col-md-4">
+          <h5 class="ftr-col-heading">Our Services</h5>
+          <ul class="ftr-nav-links">
+            <li><a href="/">Digital Marketing</a></li>
+            <li><a href="/seo-services-company-in-dubai/">Search Engine Optimization</a></li>
+            <li><a href="/social-media-marketing-agency-in-dubai/">Social Media Marketing</a></li>
+            <li><a href="/pay-per-click-ppc-services-in-dubai/">Pay-Per-Click ads</a></li>
+            <li><a href="/email-marketing-company-in-dubai/">Email Marketing</a></li>
+            <li><a href="/branding-agency-in-dubai/">Branding Services</a></li>
+            <li><a href="/website-design-company-in-dubai/">Web Design & Development</a></li>
+            <li><a href="/ui-ux-design-company-in-dubai/">UI UX Design</a></li>
+          </ul>
+        </div>
+
+        <!-- Col 4: Contact Us -->
+        <div class="col-lg-3 col-md-4">
+          <h5 class="ftr-col-heading">Contact Us</h5>
+          <div class="ftr-contact-list">
+            <div class="ftr-contact-item">
+              <svg class="ftr-contact-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/></svg>
+              <a href="tel:+971522831655">+971 52 283 1655</a>
+            </div>
+            <div class="ftr-contact-item">
+              <svg class="ftr-contact-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></svg>
+              <a href="mailto:info@brandstory.ae">info@brandstory.ae</a>
+            </div>
+            <div class="ftr-contact-item">
+              <svg class="ftr-contact-icon ftr-contact-icon-addr" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"/><circle cx="12" cy="10" r="3"/></svg>
+              <a href="https://g.co/kgs/4bHRtr4" target="_blank" rel="noopener">G5, Al Meheri Plaza, opp DBC Building, Al Khabaisi Area, Deira Dubai - 81577, United Arab Emirates</a>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="col-6 col-md-2 col-lg-1">
-        <h5 class="ftr-sub-title">Follow us</h5>
-        <div class="linkedin"><a href="https://www.linkedin.com/company/brandstoryae/" target="_blank"
-            alt="Linkedin">Linkedin</a></div>
-        <div class="youtube"><a href="https://www.youtube.com/@brandstoryuae7649"
-            target="_blank" alt="Youtube">Youtube</a></div>
-        <div class="fb"><a href="https://www.facebook.com/brandstoryuae/" target="_blank"
-            alt="Facebook">Facebook</a></div>
-        <div class="insta"><a href="https://www.instagram.com/brandstorydubai/" target="_blank"
-            alt="Instagram">Instagram</a></div>
-        <div class="twit"><a href="https://x.com/BrandStory_UAE" target="_blank" alt="Twitter">Twitter</a></div>
+    </div>
+  </div>
+
+  <!-- Footer Copyright Bar -->
+  <div class="footer-copyright-bar">
+    <div class="container">
+      <div class="copyright-text">
+        All Rights Reserved © BrandStory 2012-<?php echo date('Y'); ?> | <a href="/terms-and-conditions/">Terms & Conditions</a> | <a href="/privacy-policy/">Privacy Policy</a>
       </div>
     </div>
-  </div><!--Container end-->
-</section><!--Footer top end-->
-<section class="footer-btm"><!--Footer bottom start-->
-  <div class="container"><!--Container Start-->
-    <div class="row align-items-center">
-      <div class="col-md-12">
-        <div class="copy">All Rights Reserved © BrandStory 2012-<?php echo date('Y'); ?> | <a
-            href="/terms-and-conditions/">Terms & Conditions</a> | <a
-            href="/privacy-policy/">Privacy Policy</a></div>
-      </div>
-    </div>
-  </div><!--Container end-->
-</section><!--Footer bottom end-->
+  </div>
+
+  <!-- Footer Giant Banner -->
+  <div class="footer-giant-banner">
+    <h1 class="giant-brand-title">BRANDSTORY</h1>
+  </div>
+</footer>
 
 <a href="javascript:" id="return-to-top" aria-label="Return to top"><span class="upIcon"></span></a>
 </div><!-- Page Content End-->
